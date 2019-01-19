e13859dee10654cc0e70c94ca5d7c63af8532ed8
Michael <michaeljaison.me@gmail.com>
2019-01-19 12:20:39 -0500

    Added package and node modelues to the project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 COMP308-W2019-Lesson2B
+
+
+npm init
+npm start
+
+//Dev dependencies - only for development
+npm install @types/node --save-dev
---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+
+//creational design patter or factory desing pattern
+const server = http.createServer(function(req, res){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    //template string `
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
